552e879c2ae3940c0312404a147e7aceaf5fc7d7
kolodani <danielvinzia@gmail.com>
2023-03-01 00:44:19 -0300

    java, tipos de datos char y boolean

--- DataTypesJava/src/DataTypes.java
+++ DataTypesJava/src/DataTypes.java
@@ -6,5 +6,16 @@ public class DataTypes {
         long r = 922337036854775807L; // numero mas grande en un long, 8 Bytes, Siempre colocar una L al final
         float f = 5.46465465465465465465465465465464F; // numero con punto flotante de 4 bytes
         double d = 1.65146584984651316565146546514631685498465D; // numero con punto flotante de 8 bytes
+
+        var salary = 1000; // int
+        // pension 3%
+        var pension = salary * 0.03; // double
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Daniel Vinzia";
+        System.out.println("Employee: " + employeeName + " Salary: " + totalSalary);
     }
 }
