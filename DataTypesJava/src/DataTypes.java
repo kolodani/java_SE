b61a0850e4f7b9ac4430551ad52a27483d4acb3b
kolodani <danielvinzia@gmail.com>
2023-03-01 00:32:39 -0300

    java, tipos de datos numericos

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte a = 127; // numero mas grande de un byte, 1 byte
+        short b = 32767; // numeri mas grande de un short, 2 bytes
+        int n = 2147483647; // numero mas grande en un int, 4 bytes
+        long r = 922337036854775807L; // numero mas grande en un long, 8 Bytes, Siempre colocar una L al final
+        float f = 5.46465465465465465465465465465464F; // numero con punto flotante de 4 bytes
+        double d = 1.65146584984651316565146546514631685498465D; // numero con punto flotante de 8 bytes
+    }
+}
