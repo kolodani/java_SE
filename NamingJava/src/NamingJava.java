2a8c238387a8143780ae0aae2f672337e0e5dd03
kolodani <danielvinzia@gmail.com>
2023-03-01 00:05:27 -0300

    java, nombrando variables

---
+++ NamingJava/src/NamingJava.java
@@ -0,0 +1,18 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 33337777;
+        int celPhone = 55558888;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Argentina";
+        String _backgroundColor = "Red";
+
+        String Currency$ = "MXN";
+        String background_color = "Blue";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+    }
+}
