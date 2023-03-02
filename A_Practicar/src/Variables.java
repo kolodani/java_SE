5dd1ec158f3aa9e5bb2ca0a8c9a5c75ff4462ba3
kolodani <danielvinzia@gmail.com>
2023-03-02 01:07:35 -0300

    java, ejercicio numero uno de A Practicar

---
+++ A_Practicar/src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        String mama = "Elba";
+        String papa = "Esteban";
+        String hermano = "Miguel Angel";
+        String novia = "Macarena";
+        String yo = "Daniel";
+        String apellido = "Vinzia";
+        int edad = 33;
+        System.out.println("Hola me llamo " + yo +
+        ", Vivo en Rosario con mi novia " + novia +
+        ". Mi apellido es " + apellido + " y tengo "
+        + edad + " años." +  " Mis papas " + papa + " y "
+        + mama + " viven en Chañar ladeado." + " Y mi hermano "
+        + hermano + " vive con su novia");
+    }
+}
